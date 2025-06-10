29896776099fbeb727e04ea918b591fcba6c9576
Divyanshu Singh <ds7646106@gmail.com>
2025-06-10 09:39:42 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,10 +5,14 @@ public class DBConnection {
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/chatbot_db", "root", "root");
+            return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/chatbot_db", "root", "root"
+            );
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Driver not found. Please check your library setup.");
         } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+            System.out.println("Failed to connect to database. Please check DB URL, username, or password.");
         }
+        return null;
     }
 }
