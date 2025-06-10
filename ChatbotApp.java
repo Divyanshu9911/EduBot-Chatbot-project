4d8f14d6b41b03b5304c2c97b85311cd071ac6c0
Divyanshu Singh <ds7646106@gmail.com>
2025-06-10 09:33:43 +0530

    Update ChatbotApp.java

--- ChatbotApp.java
+++ ChatbotApp.java
@@ -23,11 +23,34 @@ public class ChatbotApp {
 
         askButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String question = inputField.getText();
-                ChatbotDAO dao = new ChatbotDAO();
-                String answer = dao.getAnswer(question);
-                outputArea.append("You: " + question + "\n");
-                outputArea.append("Bot: " + answer + "\n\n");
+                String question = inputField.getText().trim();
+
+                if (question.isEmpty()) {
+                    JOptionPane.showMessageDialog(frame, "Please enter something!", "Input Error", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                if (question.equalsIgnoreCase("exit")) {
+                    JOptionPane.showMessageDialog(frame, "Thank you for using EduBot!", "Exit", JOptionPane.INFORMATION_MESSAGE);
+                    System.exit(0);
+                }
+
+                if (question.equalsIgnoreCase("help")) {
+                    String helpMsg = "You can ask:\n- What is the fee?\n- Who is the HOD?\n- Exam date?\n- Contact info\n- Time table?";
+                    JOptionPane.showMessageDialog(frame, helpMsg, "Help", JOptionPane.INFORMATION_MESSAGE);
+                    return;
+                }
+
+                try {
+                    ChatbotDAO dao = new ChatbotDAO();
+                    String answer = dao.getAnswer(question);
+                    outputArea.append("You: " + question + "\n");
+                    outputArea.append("Bot: " + answer + "\n\n");
+                } catch (Exception ex) {
+                    outputArea.append("Bot: Sorry, something went wrong. Please try again later.\n\n");
+                    ex.printStackTrace();
+                }
+
                 inputField.setText("");
             }
         });
